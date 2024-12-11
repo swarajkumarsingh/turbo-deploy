7955e3ceb78ee437455be24e37b443b8d7a9fb23
swarajkumarsingh <sswaraj169@gmail.com>
2024-12-11 14:29:59 +0530

    Implemented: env variable

--- service/reverse-proxy/nodejs/index.js
+++ service/reverse-proxy/nodejs/index.js
@@ -1,11 +1,11 @@
+require("dotenv").config();
 const express = require("express");
 const httpProxy = require("http-proxy");
 
 const app = express();
 const PORT = 8000;
 
-const BASE_PATH =
-  "https://turbo-deploy-s3.s3.ap-south-1.amazonaws.com/__outputs";
+const BASE_PATH = process.env.BASE_PATH;
 
 const proxy = httpProxy.createProxy();
 
@@ -13,13 +13,11 @@ app.use((req, res) => {
   const hostname = req.hostname;
   const subdomain = hostname.split(".")[0];
 
-  // Custom Domain - DB Query
-  // DB Query = prisma.
+  // subdomain -> get the latest deploymentID(status: READY)
 
-  // kafka event page visit
+  // IMP analytics data to SQS -> push to aws sqs
 
   const resolvesTo = `${BASE_PATH}/${subdomain}`;
-
   return proxy.web(req, res, { target: resolvesTo, changeOrigin: true });
 });
 
