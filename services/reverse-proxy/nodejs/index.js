22f3ca14401e0a1d1f441f5e1e628f5de38cacbb
swarajkumarsingh <sswaraj169@gmail.com>
2024-12-23 13:43:27 +0530

    Implemented: Dynamic subdomain routing

--- services/reverse-proxy/nodejs/index.js
+++ services/reverse-proxy/nodejs/index.js
@@ -1,24 +1,61 @@
 require("dotenv").config();
+const { Pool } = require("pg");
 const express = require("express");
 const httpProxy = require("http-proxy");
 
-const app = express();
 const PORT = 8000;
-
+const app = express();
 const BASE_PATH = process.env.BASE_PATH;
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  ssl: { rejectUnauthorized: false },
+});
 
 const proxy = httpProxy.createProxy();
 
-app.use((req, res) => {
-  const hostname = req.hostname;
-  const subdomain = hostname.split(".")[0];
+app.use(async (req, res) => {
+  try {
+    const hostname = req.hostname;
+    const subdomain = hostname.split(".")[0];
 
-  // subdomain -> get the latest deploymentID(status: READY)
+    const projectQuery = `
+      SELECT id 
+      FROM projects 
+      WHERE subdomain = $1 
+      LIMIT 1;
+    `;
+    const projectResult = await pool.query(projectQuery, [subdomain]);
 
-  // IMP analytics data to SQS -> push to aws sqs
+    if (projectResult.rows.length === 0) {
+      return res.status(404).send("Subdomain not found");
+    }
+    const projectId = projectResult.rows[0].id;
 
-  const resolvesTo = `${BASE_PATH}/${subdomain}`;
-  return proxy.web(req, res, { target: resolvesTo, changeOrigin: true });
+    const deploymentQuery = `
+      SELECT id 
+      FROM deployments 
+      WHERE project_id = $1 AND status = 'READY' 
+      ORDER BY created_at DESC 
+      LIMIT 1;
+    `;
+    const deploymentResult = await pool.query(deploymentQuery, [projectId]);
+
+    if (deploymentResult.rows.length === 0) {
+      return res.status(404).send("No deployment found with status 'READY'");
+    }
+    const deploymentId = deploymentResult.rows[0].id;
+
+    const resolvesTo = `${BASE_PATH}/${deploymentId}`;
+    return proxy.web(req, res, { target: resolvesTo, changeOrigin: true });
+  } catch (err) {
+    console.error("Error handling request:", err);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 proxy.on("proxyReq", (proxyReq, req, res) => {
@@ -26,4 +63,4 @@ proxy.on("proxyReq", (proxyReq, req, res) => {
   if (url === "/") proxyReq.path += "index.html";
 });
 
-app.listen(PORT, () => console.log(`Reverse Proxy Running..${PORT}`));
+app.listen(PORT, () => console.log(`Reverse Proxy Running on Port ${PORT}`));
