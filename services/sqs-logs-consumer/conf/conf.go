8aebbd4d981d9f8f127b6c317d184c6c4c9b7ef8
swarajkumarsingh <sswaraj169@gmail.com>
2024-12-24 22:07:56 +0530

    fix: env variables conflict for Logs Server

--- services/sqs-logs-consumer/conf/conf.go
+++ services/sqs-logs-consumer/conf/conf.go
@@ -2,13 +2,11 @@ package conf
 
 import "os"
 
-var AWS_TOKEN = os.Getenv("AWS_TOKEN")
-var AWS_REGION = ""
-var AWS_SQS_URL = ""
-var AWS_ACCESS_KEY = ""
-var AWS_SECRET_ACCESS_KEY = ""
-var DB_URL=""
-
+var DB_URL = os.Getenv("DB_URL")
+var AWS_REGION = os.Getenv("AWS_REGION")
+var AWS_SQS_URL = os.Getenv("AWS_SQS_URL")
+var AWS_ACCESS_KEY = os.Getenv("AWS_ACCESS_KEY")
+var AWS_SECRET_ACCESS_KEY = os.Getenv("AWS_SECRET_ACCESS_KEY")
 
 const VisibilityTimeout = 10
 const MaxNumberOfMessages = 10
